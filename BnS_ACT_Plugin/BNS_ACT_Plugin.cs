5dd2fab0ea3bd3fc1a86cce2ad7d174c0a6a6752
TerriblePuns <pyronus.pyro@gmail.com>
2016-09-14 23:23:11 -0600

    Update memory offset for the Legendary Pet (2016/09/14) patch.

--- BnS_ACT_Plugin/BNS_ACT_Plugin.cs
+++ BnS_ACT_Plugin/BNS_ACT_Plugin.cs
@@ -335,7 +335,7 @@ namespace BNS_ACT_Plugin
             }
         }
 
-        private const Int32 chatlogOffset = 0x00E465FC;
+        private const Int32 chatlogOffset = 0x00E4762C;
 
         private static void Scan()
         {
